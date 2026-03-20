01ac5a4625b23a65019703872efada181424ec08
Leyton Addison-Roach <git@laddisonroach.com>
2026-03-20 01:49:21 +0000

    refactor: order enums alphabetically

--- src/datastage_py/enums.py
+++ src/datastage_py/enums.py
@@ -3,22 +3,18 @@
 from enum import IntEnum, IntFlag, StrEnum
 
 
-class JobStatus(IntEnum):
-    """Maps ``jobStatus`` values from ``DSJOBINFO``."""
+class CustInfoType(IntEnum):
+    """Maps ``infoType`` values from ``DSCUSTINFO``."""
 
-    RUNNING = 0
-    RUN_OK = 1
-    RUN_WARN = 2
-    RUN_FAILED = 3
-    QUEUED = 4
-    VAL_OK = 11
-    VAL_WARN = 12
-    VAL_FAILED = 13
-    RESET = 21
-    CRASHED = 96
-    STOPPED = 97
-    NOT_RUNNABLE = 98
-    NOT_RUNNING = 99
+    VALUE = 1
+    DESC = 2
+
+
+class EnvVarType(StrEnum):
+    """Maps ``varType`` values from ``DSAddEnvVar``."""
+
+    STRING = "String"
+    ENCRYPTED = "Encrypted"
 
 
 class JobInfoType(IntEnum):
@@ -49,6 +45,73 @@ class JobInfoType(IntEnum):
     RESTARTABLE = 24
 
 
+class JobStatus(IntEnum):
+    """Maps ``jobStatus`` values from ``DSJOBINFO``."""
+
+    RUNNING = 0
+    RUN_OK = 1
+    RUN_WARN = 2
+    RUN_FAILED = 3
+    QUEUED = 4
+    VAL_OK = 11
+    VAL_WARN = 12
+    VAL_FAILED = 13
+    RESET = 21
+    CRASHED = 96
+    STOPPED = 97
+    NOT_RUNNABLE = 98
+    NOT_RUNNING = 99
+
+
+class LimitType(IntEnum):
+    """Maps ``limitType`` values from ``DSSetJobLimit``."""
+
+    WARN = 1
+    ROWS = 2
+
+
+class LinkInfoType(IntEnum):
+    """Maps ``infoType`` values from ``DSLINKINFO``."""
+
+    LAST_ERROR = 1
+    NAME = 2
+    ROW_COUNT = 3
+    SQL_STATE = 4
+    DBMS_CODE = 5
+    DESC = 6
+    STAGE = 7
+    INST_ROW_COUNT = 8
+    EOT_ROW_COUNT = 9
+    EXT_ROW_COUNT = 10
+
+
+class LogEventType(IntEnum):
+    """Maps ``eventType`` values from ``DSLOGDETAILFULL``."""
+
+    INFO = 1
+    WARNING = 2
+    FATAL = 3
+    REJECT = 4
+    STARTED = 5
+    RESET = 6
+    BATCH = 7
+    OTHER = 98
+    ANY = 99
+
+
+class ParamType(IntEnum):
+    """Maps ``paramType`` values from ``DSPARAM``."""
+
+    STRING = 0
+    ENCRYPTED = 1
+    INTEGER = 2
+    FLOAT = 3
+    PATHNAME = 4
+    LIST = 5
+    DATE = 6
+    TIME = 7
+
+
 class ProjectInfoType(IntEnum):
     """Maps ``infoType`` values from ``DSPROJECTINFO``."""
 
@@ -58,6 +121,63 @@ class ProjectInfoType(IntEnum):
     INSTALL_TAG = 4
     TCP_PORT = 5
     PATH = 6
+
+
+class ProjectProperty(StrEnum):
+    """Maps ``property`` values from ``DSSetProjectProperty``."""
+
+    OSH_VISIBLE_FLAG = "OSHVisibleFlag"
+    JOB_ADMIN_ENABLED = "JobAdminEnabled"
+    RTCP_ENABLED = "RTCPEnabled"
+    PROTECTION_ENABLED = "ProtectionEnabled"
+    PX_ADVANCED_RUNTIME_OPTS = "PXAdvRTOptions"
+    PX_DEPLOY_CUSTOM_ACTION = "PXDeployCustomAction"
+    PX_DEPLOY_JOBDIR_TEMPLATE = "PXDeployJobDirectoryTemplate"
+    PX_BASEDIR = "PXRemoteBaseDirectory"
+    PX_DEPLOY_GENERATE_XML = "PXDeployGenerateXML"
+
+
+class ReportType(IntEnum):
+    """Maps ``reportType`` values from ``DSREPORTINFO``."""
+
+    BASIC = 0
+    DETAIL = 1
+    XML = 2
+
+
+class ReposJobFilter(IntFlag):
+    """Maps ``infoType`` values from ``DSREPOSINFO``."""
+
+    SERVER = 1
+    PARALLEL = 2
+    MAINFRAME = 4
+    SEQUENCE = 8
+    ALL = 15
+
+
+class ReposObjectType(IntEnum):
+    """Maps ``infoType`` values from ``DSREPOSINFO``."""
+
+    JOBS = 1
+
+
+class ReposRelationshipType(IntEnum):
+    """Maps ``relationshipType`` values from ``DSREPOSUSAGEJOB``."""
+
+    JOB_USES_JOB = 1
+    JOB_USED_BY_JOB = 2
+    JOB_HAS_SOURCE_TABLEDEF = 3
+    JOB_HAS_TARGET_TABLEDEF = 4
+    JOB_HAS_SOURCE_OR_TARGET_TABLEDEF = 5
+
+
+class RunMode(IntEnum):
+    """Maps ``runMode`` values from ``DSRUNJOB``."""
+
+    NORMAL = 1
+    RESET = 2
+    VALIDATE = 3
+    RESTART = 4
 
 
 class StageInfoType(IntEnum):
@@ -83,128 +203,8 @@ class StageInfoType(IntEnum):
     CUST_INFO_LIST = 18
 
 
-class LinkInfoType(IntEnum):
-    """Maps ``infoType`` values from ``DSLINKINFO``."""
-
-    LAST_ERROR = 1
-    NAME = 2
-    ROW_COUNT = 3
-    SQL_STATE = 4
-    DBMS_CODE = 5
-    DESC = 6
-    STAGE = 7
-    INST_ROW_COUNT = 8
-    EOT_ROW_COUNT = 9
-    EXT_ROW_COUNT = 10
-
-
 class VarInfoType(IntEnum):
     """Maps ``infoType`` values from ``DSVARINFO``."""
 
     VALUE = 1
     DESC = 2
-
-
-class CustInfoType(IntEnum):
-    """Maps ``infoType`` values from ``DSCUSTINFO``."""
-
-    VALUE = 1
-    DESC = 2
-
-
-class LogEventType(IntEnum):
-    """Maps ``eventType`` values from ``DSLOGDETAILFULL``."""
-
-    INFO = 1
-    WARNING = 2
-    FATAL = 3
-    REJECT = 4
-    STARTED = 5
-    RESET = 6
-    BATCH = 7
-    OTHER = 98
-    ANY = 99
-
-
-class RunMode(IntEnum):
-    """Maps ``runMode`` values from ``DSRUNJOB``."""
-
-    NORMAL = 1
-    RESET = 2
-    VALIDATE = 3
-    RESTART = 4
-
-
-class ParamType(IntEnum):
-    """Maps ``paramType`` values from ``DSPARAM``."""
-
-    STRING = 0
-    ENCRYPTED = 1
-    INTEGER = 2
-    FLOAT = 3
-    PATHNAME = 4
-    LIST = 5
-    DATE = 6
-    TIME = 7
-
-
-class ReportType(IntEnum):
-    """Maps ``reportType`` values from ``DSREPORTINFO``."""
-
-    BASIC = 0
-    DETAIL = 1
-    XML = 2
-
-
-class ReposRelationshipType(IntEnum):
-    """Maps ``relationshipType`` values from ``DSREPOSUSAGEJOB``."""
-
-    JOB_USES_JOB = 1
-    JOB_USED_BY_JOB = 2
-    JOB_HAS_SOURCE_TABLEDEF = 3
-    JOB_HAS_TARGET_TABLEDEF = 4
-    JOB_HAS_SOURCE_OR_TARGET_TABLEDEF = 5
-
-
-class ReposObjectType(IntEnum):
-    """Maps ``infoType`` values from ``DSREPOSINFO``."""
-
-    JOBS = 1
-
-
-class ReposJobFilter(IntFlag):
-    """Maps ``infoType`` values from ``DSREPOSINFO``."""
-
-    SERVER = 1
-    PARALLEL = 2
-    MAINFRAME = 4
-    SEQUENCE = 8
-    ALL = 15
-
-
-class LimitType(IntEnum):
-    """Maps ``limitType`` values from ``DSSetJobLimit``."""
-
-    WARN = 1
-    ROWS = 2
-
-
-class EnvVarType(StrEnum):
-    """Maps ``varType`` values from ``DSAddEnvVar``."""
-
-    STRING = "String"
-    ENCRYPTED = "Encrypted"
-
-
-class ProjectProperty(StrEnum):
-    """Maps ``property`` values from ``DSSetProjectProperty``."""
-
-    OSH_VISIBLE_FLAG = "OSHVisibleFlag"
-    JOB_ADMIN_ENABLED = "JobAdminEnabled"
-    RTCP_ENABLED = "RTCPEnabled"
-    PROTECTION_ENABLED = "ProtectionEnabled"
-    PX_ADVANCED_RUNTIME_OPTS = "PXAdvRTOptions"
-    PX_DEPLOY_CUSTOM_ACTION = "PXDeployCustomAction"
-    PX_DEPLOY_JOBDIR_TEMPLATE = "PXDeployJobDirectoryTemplate"
-    PX_BASEDIR = "PXRemoteBaseDirectory"
-    PX_DEPLOY_GENERATE_XML = "PXDeployGenerateXML"
